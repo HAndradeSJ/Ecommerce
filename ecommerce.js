7673c857ddf9bbe366a14d55714713728c9a84d3
HAndradeSJ <henriqeandradesj@outlook.com>
2022-05-25 20:51:16 -0300

    meu primeiro commit

---
+++ ecommerce.js
@@ -0,0 +1,9 @@
+// direcionar para pagina de login
+function login(){
+    window.location.href="http://127.0.0.1:5500/login.html";
+}
+
+// direcionar para pagina de compra
+function index(){
+    window.location.href="http://127.0.0.1:5500/index.html";
+}
