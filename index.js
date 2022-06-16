087d672fe241476867cb43cc94df6afbee3a982a
HAndradeSJ <henriqeandradesj@outlook.com>
2022-06-16 12:09:06 -0300

    menu lateral

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // função do menu
 function menu(){
-    document.getElementById('menu')
+    document.getElementById('menu').style.width = '250px';   
+}
+function menu1(){
+    document.getElementById('menu').style.width = '0px';
 }
