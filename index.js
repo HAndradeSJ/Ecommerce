6ce04d6710ae2996a3540b715c1da32b1296ec8e
HAndradeSJ <henriqeandradesj@outlook.com>
2022-06-16 12:29:54 -0300

    vs 1.2

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function menu(){
 function menu1(){
     document.getElementById('menu').style.width = '0px';
 }
+// redirecionamento da pagina
+function most1(){
+    window.location.href= "http://127.0.0.1:5500/most1.html";
+}
