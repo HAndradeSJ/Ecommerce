160533dab0e1bb28848d4013f0f89f01cc1412ad
HAndradeSJ <henriqeandradesj@outlook.com>
2022-06-17 22:08:25 -0300

    feauture finalizada

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ function login(){
 function login1(){
     document.getElementById('login').style.width ='0px';
 }
+// função do carrinho
+function carrinho(){
+    document.getElementById('carrinho').style.width = '23vw'; 
+}
+function carrinhof(){
+    document.getElementById('carrinho').style.width = '0vw';
+}
 
 // redirecionamento da pagina
 function most1(){
